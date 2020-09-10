82933115390b6d2232c2f9b97feb56979218f012
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-09-10 10:39:45 -0400

    fixed some typos

--- queries.sql
+++ queries.sql
@@ -3,27 +3,30 @@
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
 Select p.productname, c.CategoryName
-from product as p 
-join category as c on p.categoryid = c.id;
+from products as p 
+join categories as c on p.categoryid = c.categoryid;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
-select o.id, s.companyname
-from [order] as O 
-join shipper as s 
-on o.shipvia = s.id and o.orderdate < '2012-08-09';
+select o.orderid, s.shippername as companyname
+from [orders] as O
+join shippers as s 
+on o.shipperid = s.shipperid 
+where o.orderdate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
 select p.productname, o.quantity
-from productdetails_v as p 
+from products as p 
 join orderdetails as o 
-on o.productid = p.id and o.orderid = 10251
+on o.productid = p.productid 
+where o.orderid = 10251
 order by p.productname;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
 
-select o.id as orderid, c.companyname as customerscompany, employee.lastname as employeelastname
-from [order] as o 
-JOIN "Customer" AS c
-on c.id = o.customerid and o.employeeid = employee.id;
+select o.orderid as orderid, c.customername as customerscompany, e.lastname as employeelastname
+from [orders] as o 
+JOIN customers AS c
+join employees as e
+on c.customerid = o.customerid and o.employeeid = e.employeeid;
